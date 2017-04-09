90dfee70a122d9aae421413f618fd8f964b7e2f3
xuchenhui <xuchenhui@baidu.com>
2017-04-09 17:43:47 +0800

    Add scale & line chart

---
+++ src/wx-chart.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import WxChart from './charts/wxChart';
+import WxDoughnut from '../src/charts/doughnut';
+import WxLiner from '../src/charts/liner';
+
+export { WxChart, WxDoughnut, WxLiner };
